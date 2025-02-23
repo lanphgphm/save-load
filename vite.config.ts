13bb9f5ec4285c895f7d21560eec5b68f874a5a5
lanphgphm <lanphuongpham02@gmail.com>
2025-02-23 18:45:24 +0700

    update new service port

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 3000,
     host: '127.0.0.1',
     proxy: {
-      '/graces': {
-        target: 'http://127.0.0.1:3000',
+      '/graphs': {
+        target: 'http://127.0.0.1:3002',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/graces/, '/graces'),
+        rewrite: (path) => path.replace(/^\/graphs/, '/graphs'),
         configure: (proxy, _options) => {
           proxy.on('error', (err, _req, _res) => {
             console.log('proxy error', err);
